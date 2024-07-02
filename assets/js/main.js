14cd1c34e7c7fe60e45730891f250c293c3b6fb3
padil <fadhilgani2@gmail.com>
2024-07-02 15:58:43 +0700

    melanjutkan responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,8 +22,8 @@
                           </div>
                           <div class="col-md-8">
                             <div class="card-body h-100 text-start">
-                              <h5 class="card-title fw-bold">${hasil[i].judul}</h5>
-                              <p class="card-text">
+                              <h5 class="card-title fw-bold mb-2">${hasil[i].judul}</h5>
+                              <p class="card-text" style="font-size: small;">
                                 ${hasil[i].isi}
                               </p>
                               <a href="${hasil[i].link}">
@@ -100,7 +100,7 @@
         // badge
         for (let j = 0; j < listProject[i].badge.length; j++) {
           listProject[i].badge[j] = `
-            <p class="badge bg-body-secondary text-black fw-normal">
+            <p class="badge mb-0 bg-body-secondary text-black fw-normal">
               ${listProject[i].badge[j]}
             </p>
           `;
@@ -141,103 +141,126 @@
         // html
         listJobArea.innerHTML += `
                             <div class="mx-auto mt-md-4">
-                            <div class="shadow px-3 mb-3 p-3 p-sm-4 rounded">
-                              <div
-                                class="row justify-content-between align-items-start"
-                              >
-                                <!-- profil -->
+                        <div class="shadow px-3 mb-3 p-3 p-sm-4 rounded">
+                          <div
+                            class="row justify-content-between align-items-start"
+                          >
+                            <!-- profil -->
+                            <div
+                              class="col-12 col-sm-4 col-md-3 col-lg-2 left text-center mb-3 d-none d-md-block"
+                            >
+                              <img
+                                src="${listProject[i].fotoProfil}"
+                                alt=""
+                                class="img-thumbnail rounded-circle w-50"
+                              />
+                              <div class="d-block mt-2">${
+                                listProject[i].nama
+                              }</div>
+                            </div>
+
+                            <!-- right -->
+                            <div class="col-12 col-md-9 col-lg-10 right">
+                              <div class="row">
                                 <div
-                                  class="col-12 col-sm-4 col-md-3 col-lg-2 left text-center mb-3 d-none d-md-block"
+                                  class="col-12 col-lg-7 ps-0 pe-0 mb-2 d-flex align-items-center justify-content-between"
                                 >
-                                  <img
-                                    src="${listProject[i].fotoProfil}"
-                                    alt=""
-                                    class="img-thumbnail rounded-circle w-50"
-                                  />
-                                  <div class="d-block mt-2">${
-                                    listProject[i].nama
-                                  }</div>
-                                </div>
-    
-                                <!-- right -->
-                                <div class="col-12 col-md-9 col-lg-10 right">
-                                  <div class="row">
-                                    <div
-                                      class="col-12 col-lg-7 ps-0 mb-2 d-flex align-items-center justify-content-between"
-                                    >
-                                      <!-- left -->
-                                      <div>
-                                        <h6 class="fw-semibold">
-                                          ${listProject[i].judul}
-                                        </h6>
-                                        <p class="d-block fw-semibold">
-                                          Anggaran ${listProject[i].harga}
-                                        </p>
-                                      </div>
-                                    </div>
-    
-                                    <!-- right -->
-                                    <div
-                                      class="col-12 col-lg-5 mt-2 mt-lg-0 d-none d-md-block pe-0"
-                                    >
-                                      <div
-                                        class="d-flex handle-flex justify-content-lg-end gap-2"
-                                      >
-                                       ${listProject[i].penawaran}
-                                      </div>
-                                    </div>
-                                  </div>
-    
-                                  <!-- deskripsi -->
-                                  <div
-                                    class="mt-3 mt-lg-0 mb-3 d-flex flex-column gap-1"
-                                  >
-                                    <p style="font-size: 13px">
-                                      ${listProject[i].deskripsi}
+                                  <!-- left -->
+                                  <div class="me-2">
+                                    <h6 class="fw-semibold">${
+                                      listProject[i].judul
+                                    }</h6>
+                               Search     <p class="d-block fw-semibold" style="font-size: smaller;">
+                                      Anggaran ${listProject[i].harga}
                                     </p>
                                   </div>
-    
-                                  <!-- badge -->
-                                  <div class="d-flex flex-column">
-                                    <div
-                                      class="mt-md-3 d-flex align-items-center justify-content-between gap-2"
-                                    >
-                                      <div class="d-flex flex-wrap gap-1">
-                                        ${listProject[i].badge.join("")}
-                                      </div>
-                                    </div>
-                                  </div>
-    
-                                  <!-- status -->
+                                  <p class="badge text-bg-success d-md-none">Open</p>
+                                </div>
+
+                                <!-- right -->
+                                <div
+                                  class="col-12 col-lg-5 mt-2 mt-lg-0 d-none d-md-block pe-0"
+                                >
                                   <div
-                                    class="d-flex flex-column flex-md-row align-items-md-center justify-content-between pt-md-5"
+                                    class="d-flex handle-flex justify-content-lg-end gap-2"
+                                  >
+                                    ${listProject[i].penawaran}
+                                  </div>
+                                </div>
+                              </div>
+
+                              <!-- deskripsi -->
+                              <div
+                                class="mt-3 mt-lg-0 mb-3 d-flex flex-column gap-1"
+                              >
+                                <p style="font-size: 13px">
+                                  ${listProject[i].deskripsi}
+                                </p>
+                              </div>
+
+                              <!-- badge -->
+                              <div class="d-flex flex-column">
+                                <div
+                                  class="mt-md-3 d-flex align-items-center justify-content-between gap-2"
+                                >
+                                  <div class="d-flex flex-wrap gap-1">
+                                    ${listProject[i].badge.join("")}
+                                  </div>
+                                   <div class="d-md-none text-nowrap">
+                                    <p class="m-auto" style="font-size: 12px;">${listProject[i].tanggal}</p>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <hr class="d-md-none" />
+                              <!-- status -->
+                              <div
+                                class="d-flex flex-column flex-md-row align-items-md-center justify-content-between pt-md-5"
+                              >
+                                <div
+                                  class="d-none d-md-flex align-items-center gap-2 mb-3 mb-md-0"
+                                >
+                                  <p>status :</p>
+                                  <p class="badge text-bg-success">
+                                    ${listProject[i].status}
+                                  </p>
+                                </div>
+                                <div
+                                  class="d-flex align-items-center justify-content-between"
+                                >
+                                  <div class="me-3 d-none d-md-block">
+                                    ${listProject[i].tanggal}
+                                  </div>
+                                  <div
+                                    class="right d-flex justify-content-between"
                                   >
                                     <div
-                                      class="d-none d-md-flex align-items-center gap-2 mb-3 mb-md-0"
+                                      class="d-flex d-md-none align-items-center gap-2" style="width: 60%;"
                                     >
-                                      <p>status :</p>
-                                      <p class="badge text-bg-success">${
-                                        listProject[i].status
+                                      <img
+                                        src="${listProject[i].fotoProfil}"
+                                        alt=""
+                                        style="width: 30%"
+                                        class="img-thumbnail rounded-circle"
+                                      />
+                                      <p class="d-block my-auto">${
+                                        listProject[i].nama
                                       }</p>
                                     </div>
-                                    <div
-                                      class="d-flex align-items-center justify-content-between"
+                                    <button
+                                      type="button"
+                                      class="btn-primary text-white rounded my-auto"
+                                      style="font-size: small;"
                                     >
-                                      <div class="me-3 d-none d-md-block">
-                                        ${listProject[i].tanggal}
-                                      </div>
-                                      <button
-                                        type="button"
-                                        class="btn-primary text-white rounded"
-                                      >
-                                        Lihat Proyek
-                                      </button>
-                                    </div>
+                                      Lihat Proyek
+                                    </button>
                                   </div>
                                 </div>
                               </div>
                             </div>
                           </div>
+                        </div>
+                      </div>
       `;
       }
     });
