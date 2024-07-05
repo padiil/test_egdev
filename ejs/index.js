ee4bc0bb5a1bca9b6e4541701f8e60295beafd85
padil <fadhilgani2@gmail.com>
2024-07-05 17:53:21 +0700

    change pages to .ejs

--- ejs/index.js
+++ ejs/index.js
@@ -16,9 +16,20 @@ app.get("/home", (req, res) => {
 });
 
 app.get("/cek-turnitin", (req, res) => {
-    res.render("pages/cek-turnitin/cek-turnitin.ejs");
-  });
+  res.render("pages/cek-turnitin/cek-turnitin.ejs");
+});
 
+app.get("/parafrase-in", (req, res) => {
+  res.render("pages/parafrase-in/parafrase-in.ejs");
+});
+
+app.get("/translate-in", (req, res) => {
+  res.render("pages/translate-in/translate-in.ejs");
+});
+
+app.get("/sarang-freelance", (req, res) => {
+  res.render("pages/sarang-freelance/sarang-freelance.ejs");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
